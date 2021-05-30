f0cb6785415bed4e7649c6c84d190841240e485b
Daisy <ramired2@oregonstate.edu>
2021-05-29 17:45:08 -0700

    implement check divisible by 3 evenly

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,7 +1,9 @@
 def check(x):
-    msg = ""
-
     if x % 5 == 0 and x % 3 == 0:
         msg = "FizzBuzz"
+        return msg
 
-    return msg
+def check3(x):
+    if x % 3 == 0 and x % 5 != 0:
+        msg = "Fizz"
+        return msg
