fc00cc27f9548a740f21df2f3483e2fe5f780327
Daisy <ramired2@oregonstate.edu>
2021-05-29 17:36:57 -0700

    test to check if a nunber evenly divisible by 3

--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -4,7 +4,9 @@ import fizzbuzz
 class testCaseFizzbuzz(unittest.TestCase):
     def test_Divisible3and5(self):
         self.assertEqual(fizzbuzz.check(15), "FizzBuzz")
-
+    
+    def test_Divisible3Only(self):
+        self.assertEqual(fizzbuzz.check3(33), "Fizz")
 
 
 if __name__ == "__main__":
