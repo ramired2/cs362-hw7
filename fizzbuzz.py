f6ad9fac1dee8fa8ac798b14cf567d7618389c1b
Daisy <ramired2@oregonstate.edu>
2021-05-29 17:55:45 -0700

    added loop to implement programme

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,9 +1,31 @@
 def check(x):
+    msg = ""
     if x % 5 == 0 and x % 3 == 0:
         msg = "FizzBuzz"
-        return msg
+        print(msg)
+    return msg
 
 def check3(x):
+    msg = ""
     if x % 3 == 0 and x % 5 != 0:
         msg = "Fizz"
-        return msg
+        print(msg)
+    return msg
+
+def check5(x):
+    msg = ""
+    if x % 3 != 0 and x % 5 == 0:
+        msg = "Buzz"
+        print(msg)
+    return msg
+
+count = 0
+while count < 100:
+    count += 1
+
+    res = check(count)
+    res1 = check3(count)
+    res2 = check5(count)
+
+    if res == "" and res1 == "" and res2 == "":
+        print (count)
