ef17dddc37ee20a560c3290bb4d67a318c5ca647
Daisy <ramired2@oregonstate.edu>
2021-05-29 18:32:51 -0700

    Test case for a leap year divisible by 400 but not 100

--- test_leapyear.py
+++ test_leapyear.py
@@ -5,6 +5,9 @@ class testCaseFizzbuzz(unittest.TestCase):
     def test_leapDivisible4(self):
         self.assertEqual(leapyear.check(), "Leap Year")
 
+    def test_leapyear(self):
+        self.assertEqual(leapyear.sneakyLeap(), "Leap Year")
+
 
 if __name__ == "__main__":
     unittest.main()
